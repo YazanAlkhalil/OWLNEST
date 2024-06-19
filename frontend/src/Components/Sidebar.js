fa0bc943a6e49b09bbc6ef0c9acfc9ccb099d0a4
mootaz20 <mootazalhalak695@gmail.com>
2024-06-20 02:15:30 +0300

    auth edit

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,9 +1,9 @@
-import image from "../images/OIG2 3.png";
+import image from "../images/logo.png";
 
 function Sidebar({ links }) {
   return (
     <div className="flex flex-col items-center text-background  flex-grow bg-primary">
-      <img className="box-border w-[90%] max-h-60" src={image} />
+      <img className="box-border pt-4 w-[70%] mb-5 max-h-60" src={image} />
       <ul className='px-4'>
         {links.map((link) => (
           <li className="text-3xl mb-10" key={link.name}>
