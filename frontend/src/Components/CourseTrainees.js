09f6b58a42aedeab009a5e44fd01b3dbdd9f3580
Yazan <yazansamer123qwe@gmail.com>
2024-06-20 10:14:06 +0300

    done the styling for some admin pages and some trainer pages

--- frontend/src/Components/CourseTrainees.js
+++ frontend/src/Components/CourseTrainees.js
@@ -1,9 +1,60 @@
 import React from 'react'
-
+import BasicTable from './Table'
+import image from '../images/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.avif'
 function CourseTrainees() {
   return (
-    <div>
-      Trainees
+    <div className='text-2xl'>
+      <BasicTable heading={['Name','Progress','XP','Grades','Completed']} 
+      data={[
+        {
+          progress: '100%',
+          xp:"234",
+          grades:"75%",
+          completed: "2024/2/2",
+          name:"يعقوب قمر الدين ديبيازة",
+
+        },
+        {
+          progress: '50%',
+          xp:"234",
+          grades:"75%",
+          completed: null,
+          name:"خالد كشميري",
+
+        },
+        {
+          progress: '50%',
+          xp:"234",
+          grades:"75%",
+          completed: null,
+          name:"خضر كرويتا"
+
+        },
+        {
+          progress: '100%',
+          xp:"234",
+          grades:"75%",
+          completed: "2024/2/2",
+          name:"اسماعيل احمد كنباوي"
+
+        },
+        {
+          progress: '100%',
+          xp:"234",
+          grades:"75%",
+          completed: "2024/2/2",
+          name:"عثمان عبد الجليل ششة"
+
+        },
+        {
+          progress: '100%',
+          xp:"234",
+          grades:"75%",
+          completed: "2024/2/2",
+          name:"محمد سنبل"
+        },
+      ]}
+      />
     </div>
   )
 }
--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,14 +1,16 @@
+import { NavLink } from "react-router-dom";
 import image from "../images/OIG2 3.png";
 
 function Sidebar({ links }) {
+
   return (
-    <div className="flex flex-col items-center text-background  flex-grow bg-primary">
+    <div className="col-span-1 flex flex-col items-center text-background  flex-grow bg-primary flex-shrink-0">
       <img className="box-border w-[90%] max-h-60" src={image} />
-      <ul className='px-4'>
+      <ul className='px-4 flex flex-col'>
         {links.map((link) => (
-          <li className="text-3xl mb-10" key={link.name}>
-            <a href={link.url}>{link.name}</a>
-          </li>
+          <NavLink to={link.url} relative="path" className="text-3xl mb-10" key={link.name}>
+            {link.name}
+          </NavLink>
         ))}
       </ul>
     </div>
