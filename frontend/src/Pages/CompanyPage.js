0a7756628dd4d10056505003d2eed079470cf6bb
Yazan <yazansamer123qwe@gmail.com>
2024-06-25 21:48:09 +0300

    minor fixes

--- frontend/src/Pages/CompanyPage.js
+++ frontend/src/Pages/CompanyPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import NavBar from '../Components/Navbar'
 import Company from '../Components/Company'
+import Navbar2 from '../Components/Navbar2'
 
 export default function CompanyPage() {
+  
   return (
     <>
-    <NavBar />
+    <Navbar2 />
     <div className="container mx-auto">
         <h1 className='font-black text-2xl p-8'>Companies</h1>
         <div className='flex justify-evenly flex-wrap gap-7 flex-3'>
