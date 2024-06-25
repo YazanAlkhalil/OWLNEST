bfd3e0a70d62cdc23892b18f6639d7d0843e2266
Yazan <yazansamer123qwe@gmail.com>
2024-06-25 21:47:01 +0300

    minor fixes

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -5,7 +5,7 @@ function Sidebar({ links }) {
 
   return (
     <div className="col-span-1 flex flex-col items-center text-background  flex-grow bg-primary flex-shrink-0">
-      <img className="box-border pt-4 w-[70%] mb-5 max-h-60" src={image} />
+      <img className="box-border pt-4 w-[50%] mb-5 max-h-60" src={image} />
       <ul className='px-4 flex flex-col'>
         {links.map((link) => (
           <NavLink to={link.url} relative="path" className="text-3xl mb-10" key={link.name}>
