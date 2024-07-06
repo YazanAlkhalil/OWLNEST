6b1d6c29a796e6fca344247aa00b8315d821335c
mootaz20 <mootazalhalak695@gmail.com>
2024-07-07 01:43:16 +0300

    correct login

--- frontend/src/features/Auth/LoginSlice.js
+++ frontend/src/features/Auth/LoginSlice.js
@@ -1,87 +1,66 @@
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import Cookies from 'js-cookie';
-import { jwtDecode } from 'jwt-decode';
-// import axiosInstance from "../../Components/AxiosSetup";
-
-const decodeToken = (token) => jwtDecode(token);
-
-const isTokenExpired = (token) => {
-    const decoded = decodeToken(token);
-    return decoded.exp * 1000 < Date.now();
-};
 
 export const loginUser = createAsyncThunk(
-    'loginUser',
-    async (data, { rejectWithValue }) => {
+    "login",
+    async (data, thunkAPI) => {
         try {
-            const link = "http://127.0.0.1:8000/api/login/";
-            const response = await axios.post(link, data, {
+            let loginUrl = "http://127.0.0.1:8000/api/login/";
+            const response = await axios.post(loginUrl, data, {
+                withCredentials: true,
                 headers: { "Content-Type": "application/json" }
             });
-            const res = response.data;
+            let res = await response.data;
             if (response.status === 200) {
-                console.log("login successfully", res);
-                const { accessToken, refreshToken } = response.data;
-                Cookies.set('accessToken', accessToken);
-                Cookies.set('refreshToken', refreshToken);
-                return res;
+                console.log(res);
             } else {
-                console.log("login Failed:", res);
-                return rejectWithValue(res);
+                return thunkAPI.rejectWithValue(res);
             }
-        } catch (error) {
-            return rejectWithValue(error.response.data);
+        } catch (e) {
+            console.log("Error", e.response.data);
+            return thunkAPI.rejectWithValue(e.response.data);
         }
+        
     }
 );
-
-export const refreshAccessToken = createAsyncThunk(
-    'refreshAccessToken',
-    async (_, { rejectWithValue }) => {
+export const logout = createAsyncThunk(
+    "logout",
+    async (_, thunkAPI) => {
         try {
-            const link = "http://127.0.0.1:8000/api/refresh/";
-            const refreshToken = Cookies.get('refreshToken');
-            if (!refreshToken || isTokenExpired(refreshToken)) {
+            let logoutUrl = "http://127.0.0.1:8000/api/logout/";
+            const response = await axios.post(logoutUrl,{},{
+                withCredentials: true,
+                headers: { "Content-Type": "application/json" }
+            });
+            let res = await response.data;
+            if (response.status === 200) {
                 Cookies.remove('accessToken');
                 Cookies.remove('refreshToken');
-                throw new Error('Refresh token expired');
+                console.log(res);
+            } else {
+                return thunkAPI.rejectWithValue(res);
             }
-            const response = await axios.post(link, { refreshToken: refreshToken });
-            const { accessToken } = response.data;
-            Cookies.set('accessToken', accessToken);
-            return accessToken;
-        } catch (error) {
-            Cookies.remove('accessToken');
-            Cookies.remove('refreshToken');
-            return rejectWithValue(error.response.data);
+        } catch (e) {
+            console.log("Error", e.response.data);
+            return thunkAPI.rejectWithValue(e.response.data);
         }
+        
     }
 );
 
 export const LoginSlice = createSlice({
     name: "login",
     initialState: {
-        accessToken: Cookies.get('accessToken') || null,
-        refreshToken: Cookies.get('refreshToken') || null,
         isFetching: false,
         isSuccess: false,
         error: null,
-    },
-    reducers : {
-        logout: (state) => {
-            Cookies.remove('accessToken');
-            Cookies.remove('refreshToken');
-            state.accessToken = null;
-            state.refreshToken = null;
-        }
     },
     extraReducers: (builder) => {
         builder
             .addCase(loginUser.fulfilled, (state, action) => {
                 state.isSuccess = true;
-                state.accessToken = action.payload.accessToken;
-                state.refreshToken = action.payload.refreshToken;
                 state.isFetching = false;
                 state.error = null;
             })
@@ -94,9 +73,23 @@ export const LoginSlice = createSlice({
                 state.isSuccess = false;
                 state.error = action.payload;
             })
-            .addCase(refreshAccessToken.fulfilled, (state, action) => {
-                state.accessToken = action.payload;
+            .addCase(logout.fulfilled, (state) => {
+                state.isSuccess = false;
+                state.isFetching = false;
+                state.error = null;
             })
+            .addCase(logout.pending, (state) => {
+                state.isFetching = true;
+                state.error = null;
+            })
+            .addCase(logout.rejected, (state, action) => {
+                state.isFetching = false;
+                state.isSuccess = false;
+                state.error = action.payload;
+            })
+            // .addCase(refreshAccessToken.fulfilled, (state, action) => {
+            //     state.accessToken = action.payload;
+            // })
             .addMatcher(
                 (action) => action.type.endsWith('/rejected'),
                 (state, action) => {
@@ -106,7 +99,6 @@ export const LoginSlice = createSlice({
     }
 });
 
-export const { logout } = LoginSlice.actions;
 
 export const loginSelector = (state) => state.login;
 export default LoginSlice.reducer;
