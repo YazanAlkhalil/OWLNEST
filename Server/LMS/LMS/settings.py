5ebd80e10e4d39e551a8c169e5f8023df9d31c9c
Sami Alomar <sami.anas.alomar.2016.king@gmail.com>
2024-07-14 21:02:59 +0300

    only name and pref in course creation

--- Server/LMS/LMS/settings.py
+++ Server/LMS/LMS/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'authentication',
     'system',
     'django_extensions', 
-
+    'drf_yasg'
 ]
 
 
@@ -101,23 +101,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'lms',
-    #     'USER': 'root',
-    #     'PASSWORD': 'moustafa@123',
-    #     'HOST': 'localhost', 
-    #     'PORT': '3306',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'LMS',
-        'USER' : 'root',
-        'PASSWORD' : '',
-        'PORT' : 3306,
-        'HOST' : '127.0.0.1',
-
-    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'lms',
