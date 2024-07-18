a48e665e7f111c44bf1fd0754e61c5d4f3d7dfab
mootaz20 <mootazalhalak695@gmail.com>
2024-07-18 19:00:30 +0300

    fix merge

--- Server/LMS/system/models/Owner_Notification.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from system.models.Owner import Owner
-from authentication.models.User import User
-class Owner_Notification(models.Model):
-      from_user = models.ForeignKey(User,on_delete=models.CASCADE )
-      owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
-      content = models.TextField()
-      is_read = models.BooleanField(default=False)
-      sent_at = models.DateField(auto_now_add=True)
