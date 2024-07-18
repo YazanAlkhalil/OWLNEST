b1c141d4f89ae524a7ab5b76fd576ca4f43af6f7
Sami Alomar <sami.anas.alomar.2016.king@gmail.com>
2024-07-18 23:37:08 +0300

    ready now

--- Server/LMS/system/models/__init__.py
+++ Server/LMS/system/models/__init__.py
@@ -1,7 +1,3 @@
 from .Additional_Resources import Additional_Resources
-<<<<<<< HEAD
 from .Trainee_Contract import Trainee_Contract
 from .Trainer_Contract_Course import Trainer_Contract_Course
-=======
-from .Trainee_Contract import Trainee_Contract
->>>>>>> sami_branch
