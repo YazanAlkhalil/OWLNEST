42dbd90f649ff70112a86f05acec87ddfe252f65
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-07-31 18:52:50 +0300

    fix get user companies

--- Server/LMS/system/permissions/IsAdmin.py
+++ Server/LMS/system/permissions/IsAdmin.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+#models 
+from system.models.Company import Company
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         # Check if the user is authenticated
@@ -9,3 +11,13 @@ class IsAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user is the admin
         return request.user.is_admin
+    
+
+
+class IsCompanyOwner(permissions.BasePermission):
+      def has_permission(self, request, view): 
+          if Company.objects.get(id = view.kwargs['company_id']).owner.user == request.user :
+              print('TRUE')
+              return True
+          print('FALSE')
+          return False 
